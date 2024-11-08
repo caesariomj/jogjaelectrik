92e1e7f9ed7b9cc3e732a065df7210f316828458
caesariomj <caesariomj7@gmail.com>
2024-11-08 14:13:02 +0700

    feat: create CategoryPermissionsSeeder

---
+++ database/seeders/CategoryPermissionsSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class CategoryPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            'create categories',
+            'edit categories',
+            'delete categories',
+            'set primary categories',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::updateOrCreate(['name' => $permission], ['name' => $permission]);
+        }
+
+        $adminRole = Role::findByName('admin');
+        $superAdminRole = Role::findByName('super_admin');
+
+        $adminRole->givePermissionTo('create categories');
+        $adminRole->givePermissionTo('edit categories');
+        $adminRole->givePermissionTo('delete categories');
+        $superAdminRole->givePermissionTo('create categories');
+        $superAdminRole->givePermissionTo('edit categories');
+        $superAdminRole->givePermissionTo('delete categories');
+        $superAdminRole->givePermissionTo('set primary categories');
+    }
+}
