cdc21fec572a1952b25a817e625c43ea8a8eb2cc
caesariomj <caesariomj7@gmail.com>
2024-11-08 09:58:31 +0700

    feat: create admin sidebar and side-link components

---
+++ resources/views/components/admin/sidebar.blade.php
@@ -0,0 +1,57 @@
+<aside
+    :class="{'-translate-x-full': !isOpen, 'translate-x-0': isOpen}"
+    class="fixed inset-y-0 left-0 z-40 h-screen w-64 transform border-e border-neutral-300 bg-white text-black transition-transform duration-300 ease-in-out lg:sticky lg:top-0 lg:translate-x-0"
+>
+    <div class="flex items-center justify-between p-4">
+        <a href="{{ route('home') }}" class="flex w-fit items-center gap-x-4">
+            <x-common.application-logo class="block h-9 w-auto fill-current text-primary" />
+            <span class="text-xl font-semibold tracking-tight">My App</span>
+        </a>
+        <button @click="toggleSidebar" class="relative rounded-full p-2 text-black hover:bg-neutral-100 lg:hidden">
+            <svg class="size-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+            </svg>
+        </button>
+    </div>
+    <div id="sidebar-nav" class="h-[calc(100%-4.3rem)] overflow-y-hidden hover:overflow-y-auto">
+        <nav class="p-4">
+            <ul class="space-y-1.5">
+                <li>
+                    <x-admin.side-link
+                        :href="route('admin.dashboard')"
+                        :active="request()->routeIs('admin.dashboard')"
+                        wire:navigate
+                    >
+                        <svg
+                            class="size-4"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        >
+                            <path d="M15.6 2.7a10 10 0 1 0 5.7 5.7" />
+                            <circle cx="12" cy="12" r="2" />
+                            <path d="M13.4 10.6 19 5" />
+                        </svg>
+                        Dashboard
+                    </x-admin.side-link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
+<div
+    x-show="isOpen"
+    x-transition:enter="transition-opacity duration-300 ease-out"
+    x-transition:enter-start="opacity-0"
+    x-transition:enter-end="opacity-100"
+    x-transition:leave="transition-opacity duration-200 ease-in"
+    x-transition:leave-start="opacity-100"
+    x-transition:leave-end="opacity-0"
+    class="fixed inset-0 z-30 bg-black/75 lg:hidden"
+    @click="isOpen = false"
+    x-cloak
+></div>
