1170064e4140650ee1aff8c81e9b21b86c8dde2d
caesariomj <caesariomj7@gmail.com>
2024-11-11 13:03:03 +0700

    feat: add required attribute to input-label and update input and input-label styling

--- resources/views/components/form/input-label.blade.php
+++ resources/views/components/form/input-label.blade.php
@@ -1,7 +1,11 @@
 @props([
     'value',
+    'required' => true,
 ])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-black']) }}>
     {{ $value ?? $slot }}
+    @if ($required)
+        <span class="text-red-500">*</span>
+    @endif
 </label>
--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -2,5 +2,5 @@
 
 <input
     @disabled($disabled)
-    {{ $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-primary focus:ring-primary']) }}
+    {{ $attributes->merge(['class' => 'rounded-md shadow-sm border-neutral-300 focus:border-primary focus:ring-primary']) }}
 />
