77a4a8c3b2c96bd0f167343d68f13721ddf45063
caesariomj <caesariomj7@gmail.com>
2024-11-11 12:02:45 +0700

    feat: add category link to admin sidebar component

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -39,6 +39,33 @@
                         Dashboard
                     </x-admin.side-link>
                 </li>
+                <li class="border-t py-2">
+                    <span class="text-xs font-semibold uppercase tracking-tight text-black/50">Produk</span>
+                </li>
+                <li>
+                    <x-admin.side-link
+                        :href="route('admin.categories.index')"
+                        :active="request()->routeIs('admin.categories.*')"
+                        wire:navigate
+                    >
+                        <svg
+                            class="size-4"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        >
+                            <path
+                                d="M12.586 2.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l8.704 8.704a2.426 2.426 0 0 0 3.42 0l6.58-6.58a2.426 2.426 0 0 0 0-3.42z"
+                            />
+                            <circle cx="7.5" cy="7.5" r=".5" fill="currentColor" />
+                        </svg>
+                        Kategori
+                    </x-admin.side-link>
+                </li>
             </ul>
         </nav>
     </div>
