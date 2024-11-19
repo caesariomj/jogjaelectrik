4109b42c6a0b9540d32e247f78cce1e85d6f6d16
caesariomj <caesariomj7@gmail.com>
2024-11-19 14:27:36 +0700

    feat: add discount link to admin sidebar component

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -139,6 +139,35 @@
                         Subkategori
                     </x-admin.side-link>
                 </li>
+                <li class="border-t py-2">
+                    <span class="text-xs font-semibold uppercase tracking-tight text-black/50">Penjualan</span>
+                </li>
+                <li>
+                    <x-admin.side-link
+                        :href="route('admin.discounts.index')"
+                        :active="request()->routeIs('admin.discounts.*')"
+                        wire:navigate
+                    >
+                        <svg
+                            class="size-4"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        >
+                            <path
+                                d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"
+                            />
+                            <path d="m15 9-6 6" />
+                            <path d="M9 9h.01" />
+                            <path d="M15 15h.01" />
+                        </svg>
+                        Diskon
+                    </x-admin.side-link>
+                </li>
             </ul>
         </nav>
     </div>
