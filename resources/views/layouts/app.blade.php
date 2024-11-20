68522ae57f4a14201d4bff6c0167dc65d5acec3c
caesariomj <caesariomj7@gmail.com>
2024-11-20 23:07:58 +0700

    feat: update app layout to include breadcrumb and alert components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,10 @@
 @section('body')
     <livewire:layout.user.navigation />
 
+    <x-common.alert />
+
+    <x-common.breadcrumb class="px-4 pt-6 md:px-6" />
+
     <main>
         @yield('content', $slot ?? '')
     </main>
