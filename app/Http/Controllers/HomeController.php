141df4c23e4a0de046b84537e0515d539e30a82e
caesariomj <caesariomj7@gmail.com>
2024-11-28 19:43:21 +0700

    feat: add productDetail method to HomeController and define home, products, and product detail routes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use Illuminate\View\View;
 
 class HomeController extends Controller
@@ -12,5 +14,31 @@ class HomeController extends Controller
     public function index(): View
     {
         return view('pages.home');
+    }
+
+    /**
+     * Displays the product detail page.
+     */
+    public function productDetail(string $slug): View
+    {
+        $product = Product::with(['subcategory.category', 'images', 'variants.combinations.variationVariant.variation'])->findBySlug($slug)->first();
+
+        if (! $product) {
+            session()->flash('error', 'Data produk tidak ditemukan.');
+
+            return redirect()->route('products');
+        }
+
+        $productRecommendations = Product::whereHas('subcategory', function ($query) use ($product) {
+            $query->where('category_id', $product->subcategory->category->id);
+        })
+            ->where('id', '!=', $product->id)
+            ->active()
+            ->limit(6)
+            ->get();
+
+        $productRecommendations = ProductResource::collection($productRecommendations)->toArray(request());
+
+        return view('pages.product-detail', compact('product', 'productRecommendations'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,16 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
+use Livewire\Volt\Volt;
 
 require __DIR__.'/user.php';
 
 require __DIR__.'/admin.php';
 
 require __DIR__.'/auth.php';
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Volt::route('/produk', 'pages.products')->name('products');
+
+Route::get('/produk/{slug}', [HomeController::class, 'productDetail'])->name('products.detail');
