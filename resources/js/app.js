14168674478dff5cde6173befe9ddb92d62cd7a3
caesariomj <caesariomj7@gmail.com>
2024-11-28 20:54:47 +0700

    refactor: extract combobox JavaScript data into a separate file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,73 +1,15 @@
 import './bootstrap';
 
 import bannerCarouselComponent from './components/bannerCarousel';
+import comboboxComponent from './components/combobox';
 import datepickerComponent from './components/datepicker';
 import dropdownComponent from './components/dropdown';
 import productSliderComponent from './components/productSlider';
 
 document.addEventListener('alpine:init', () => {
     Alpine.data('bannerCarousel', bannerCarouselComponent);
+    Alpine.data('combobox', comboboxComponent);
     Alpine.data('dropdown', dropdownComponent);
-
-    Alpine.data(
-        'combobox',
-        (
-            comboboxData = {
-                allOptions: [],
-                selectedOption: null,
-                instanceName: '',
-            },
-        ) => ({
-            options: comboboxData.allOptions,
-            selectedOption: comboboxData.selectedOption,
-            instanceName: comboboxData.instanceName,
-            isOpen: false,
-            openedWithKeyboard: false,
-
-            init() {
-                if (this.selectedOption) {
-                    const entireData = this.findDataByValue(this.options, this.selectedOption);
-                    if (entireData) {
-                        this.setSelectedOption(entireData);
-                    }
-                }
-            },
-
-            findDataByValue(data, value) {
-                return data.find((item) => item.value === value);
-            },
-
-            setSelectedOption(option) {
-                this.selectedOption = option;
-                this.isOpen = false;
-                this.openedWithKeyboard = false;
-                this.$refs.hiddenTextField.value = option.value;
-                this.$wire.handleComboboxChange(option.value, this.instanceName);
-            },
-
-            getFilteredOptions(query) {
-                this.options = comboboxData.allOptions.filter((option) =>
-                    option.label.toLowerCase().includes(query.toLowerCase()),
-                );
-                if (this.options.length === 0) {
-                    this.$refs.noResultsMessage.classList.remove('hidden');
-                } else {
-                    this.$refs.noResultsMessage.classList.add('hidden');
-                }
-            },
-
-            handleKeydownOnOptions(event) {
-                if (
-                    (event.keyCode >= 65 && event.keyCode <= 90) ||
-                    (event.keyCode >= 48 && event.keyCode <= 57) ||
-                    event.keyCode === 8
-                ) {
-                    this.$refs.searchField.focus();
-                }
-            },
-        }),
-    );
-
     Alpine.data('flatpickr', datepickerComponent);
     Alpine.data('productSlider', productSliderComponent);
 });
