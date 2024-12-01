005285ec79238b7667467d2c904b9bb9d19f8084
caesariomj <caesariomj7@gmail.com>
2024-12-02 06:44:31 +0700

    feat: add city_id foreign key and additional user information fields migration

---
+++ database/migrations/2024_12_01_123633_add_phone_address_and_postal_code_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone_number', 512)->nullable()->after('remember_token');
+            $table->text('address')->nullable()->after('phone_number');
+            $table->string('postal_code', 512)->nullable()->after('address');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('phone_number');
+            $table->dropColumn('address');
+            $table->dropColumn('postal_code');
+        });
+    }
+};
