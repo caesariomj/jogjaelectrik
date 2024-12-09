a7303c80bd5c606782a6824f49113854b4981a96
caesariomj <caesariomj7@gmail.com>
2024-12-09 08:52:45 +0700

    style: update dropdown-link component styling

--- resources/views/components/common/dropdown-link.blade.php
+++ resources/views/components/common/dropdown-link.blade.php
@@ -5,13 +5,13 @@
 @if ($href)
     <a
         href="{{ $href }}"
-        {{ $attributes->merge(['class' => 'inline-flex items-center gap-x-3 w-full px-4 py-2 text-start text-sm leading-5 text-black font-medium hover:bg-neutral-100 focus:outline-none focus:bg-neutral-100 transition duration-150 ease-in-out']) }}
+        {{ $attributes->merge(['class' => 'inline-flex items-center gap-x-3 w-full px-4 py-2 tracking-tight text-start text-sm leading-5 text-black font-medium hover:bg-neutral-100 focus:outline-none focus:bg-neutral-100 transition duration-150 ease-in-out']) }}
     >
         {{ $slot }}
     </a>
 @else
     <button
-        {{ $attributes->merge(['class' => 'inline-flex items-center gap-x-3 w-full px-4 py-2 text-start text-sm leading-5 text-black font-medium hover:bg-neutral-100 focus:outline-none focus:bg-neutral-100 transition duration-150 ease-in-out']) }}
+        {{ $attributes->merge(['class' => 'inline-flex items-center gap-x-3 w-full px-4 py-2 tracking-tight text-start text-sm leading-5 text-black font-medium hover:bg-neutral-100 focus:outline-none focus:bg-neutral-100 transition duration-150 ease-in-out']) }}
     >
         {{ $slot }}
     </button>
