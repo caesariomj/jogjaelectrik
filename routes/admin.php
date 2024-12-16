3d7e09403fe82db026b9dc900d16747559157c65
caesariomj <caesariomj7@gmail.com>
2024-12-16 08:44:32 +0700

    refactor: remove store, update, and delete admin routes from category and subcategory management

--- routes/admin.php
+++ routes/admin.php
@@ -39,21 +39,15 @@ Route::middleware(['auth', 'role:admin|super_admin'])->prefix('admin')->name('ad
     Route::prefix('manajemen-kategori')->name('categories.')->group(function () {
         Route::get('/', [CategoryController::class, 'index'])->name('index');
         Route::get('/tambah', [CategoryController::class, 'create'])->name('create');
-        Route::post('/', [CategoryController::class, 'store'])->name('store');
         Route::get('/{slug}/detail', [CategoryController::class, 'show'])->name('show');
         Route::get('/{slug}/ubah', [CategoryController::class, 'edit'])->name('edit');
-        Route::patch('/{category}', [CategoryController::class, 'update'])->name('update');
-        Route::delete('/{category}', [CategoryController::class, 'destroy'])->name('destroy');
     });
 
     Route::prefix('manajemen-subkategori')->name('subcategories.')->group(function () {
         Route::get('/', [SubcategoryController::class, 'index'])->name('index');
         Route::get('/tambah', [SubcategoryController::class, 'create'])->name('create');
-        Route::post('/', [SubcategoryController::class, 'store'])->name('store');
         Route::get('/{slug}/detail', [SubcategoryController::class, 'show'])->name('show');
         Route::get('/{slug}/ubah', [SubcategoryController::class, 'edit'])->name('edit');
-        Route::patch('/{subcategory}', [SubcategoryController::class, 'update'])->name('update');
-        Route::delete('/{subcategory}', [SubcategoryController::class, 'destroy'])->name('destroy');
     });
 
     Route::prefix('manajemen-diskon')->name('discounts.')->group(function () {
