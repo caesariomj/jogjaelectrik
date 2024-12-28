9e576a38209f649041f83b444481d9af83adcaa0
caesariomj <caesariomj7@gmail.com>
2024-12-28 19:13:45 +0700

    feat: call ProductReviewPermissionsSeeder inside DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             OrderPermissionsSeeder::class,
             PaymentPermissionsSeeder::class,
             RefundPermissionsSeeder::class,
+            ProductReviewPermissionsSeeder::class,
         ]);
     }
 }
