9fc1abd677b38f82702d9df2ee2890afb012614d
caesariomj <caesariomj7@gmail.com>
2025-01-04 04:57:55 +0700

    style: update user order detail page styling

--- resources/views/pages/user/orders/show.blade.php
+++ resources/views/pages/user/orders/show.blade.php
@@ -172,11 +172,10 @@
                             <dt class="w-full tracking-tight text-black/70 md:w-1/3">Link Pembayaran</dt>
                             <dd class="w-full md:w-2/3">
                                 <a
-                                    href=""
-                                    class="ont-medium inline-flex items-center gap-x-1 tracking-tight text-black underline transition-colors hover:text-primary"
+                                    href="{{ $order->payment->invoice_url }}"
+                                    class="inline-flex items-center gap-x-1 font-medium tracking-tight text-black underline transition-colors hover:text-primary"
                                 >
-                                    Test
-                                    {{ $order->payment->invoice_url }}
+                                    Klik disini untuk mengakses link pembayaran
                                     <svg
                                         class="size-3 shrink-0"
                                         xmlns="http://www.w3.org/2000/svg"
@@ -358,31 +357,6 @@
                         <dt class="w-full tracking-tight text-black/70 md:w-1/3">Alamat Pengiriman</dt>
                         <dd class="w-full font-medium tracking-tight text-black md:w-2/3">
                             {{ \Illuminate\Support\Facades\Crypt::decryptString($order->shipping_address) }}
-                        </dd>
-                    </div>
-                </dl>
-            </section>
-            <section class="mb-4">
-                <h2 class="mb-2 text-2xl text-black">Informasi Pelanggan</h2>
-                <dl class="grid grid-cols-1">
-                    <div class="flex flex-col items-start gap-1 border-b border-neutral-300 py-2 md:flex-row">
-                        <dt class="w-full tracking-tight text-black/70 md:w-1/3">Nama Pelanggan</dt>
-                        <dd class="w-full font-medium tracking-tight text-black md:w-2/3">{{ $order->user->name }}</dd>
-                    </div>
-                </dl>
-                <dl class="grid grid-cols-1">
-                    <div class="flex flex-col items-start gap-1 border-b border-neutral-300 py-2 md:flex-row">
-                        <dt class="w-full tracking-tight text-black/70 md:w-1/3">Nomor Telefon</dt>
-                        <dd class="w-full font-medium tracking-tight text-black md:w-2/3">
-                            {{ '0' . \Illuminate\Support\Facades\Crypt::decryptString($order->user->phone_number) }}
-                        </dd>
-                    </div>
-                </dl>
-                <dl class="grid grid-cols-1">
-                    <div class="flex flex-col items-start gap-1 border-b border-neutral-300 py-2 md:flex-row">
-                        <dt class="w-full tracking-tight text-black/70 md:w-1/3">Alamat Email</dt>
-                        <dd class="w-full font-medium tracking-tight text-black md:w-2/3">
-                            {{ $order->user->email }}
                         </dd>
                     </div>
                 </dl>
