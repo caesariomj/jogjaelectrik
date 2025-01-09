7186bb6e8d7cbd490a1d4709abbee98e6e983235
caesariomj <caesariomj7@gmail.com>
2025-01-09 09:15:58 +0700

    feat: create expireInvoice() and createRefund() methods and refactor PaymentService

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -8,12 +8,18 @@ use Illuminate\Support\Facades\Crypt;
 use Xendit\Configuration;
 use Xendit\Invoice\CreateInvoiceRequest;
 use Xendit\Invoice\InvoiceApi;
+use Xendit\Refund\CreateRefund;
+use Xendit\Refund\RefundApi;
 
 class PaymentService
 {
+    public string $secretKey;
+
     public function __construct()
     {
-        Configuration::setXenditKey(config('services.xendit.secret_key'));
+        $this->secretKey = config('services.xendit.secret_key');
+
+        Configuration::setXenditKey($this->secretKey);
     }
 
     public function createInvoice(Order $order)
@@ -93,11 +99,11 @@ class PaymentService
             'fees' => $fees,
         ];
 
-        $invoiceApi = new InvoiceApi;
+        $apiInstance = new InvoiceApi;
         $createInvoiceRequest = new CreateInvoiceRequest($params);
 
         try {
-            $result = $invoiceApi->createInvoice($createInvoiceRequest);
+            $result = $apiInstance->createInvoice($createInvoiceRequest);
 
             return [
                 'url' => $result['invoice_url'],
@@ -117,12 +123,12 @@ class PaymentService
         }
     }
 
-    public function getInvoice($invoiceId)
+    public function getInvoice(string $invoiceId)
     {
-        $invoiceApi = new InvoiceApi;
+        $apiInstance = new InvoiceApi;
 
         try {
-            $invoice = $invoiceApi->getInvoiceById($invoiceId);
+            $invoice = $apiInstance->getInvoiceById($invoiceId);
 
             return $invoice;
         } catch (\Xendit\XenditSdkException $e) {
@@ -135,6 +141,71 @@ class PaymentService
             throw new ApiRequestException(
                 logMessage: 'Unexpected error during invoice retrieval: '.$e->getMessage(),
                 userMessage: 'Terjadi kesalahan tidak terduga pada sistem pembayaran, silakan coba beberapa saat lagi.',
+                statusCode: $e->getCode()
+            );
+        }
+    }
+
+    public function expireInvoice(string $xenditInvoiceId)
+    {
+        $apiInstance = new InvoiceApi;
+
+        try {
+            $apiInstance->expireInvoice($xenditInvoiceId);
+        } catch (\Xendit\XenditSdkException $e) {
+            throw new ApiRequestException(
+                logMessage: 'Unexpected error during xendit invoice expiration: '.$e->getErrorMessage(),
+                userMessage: 'Terjadi kesalahan pada sistem pembayaran, silakan coba beberapa saat lagi.',
+                statusCode: $e->getErrorCode()
+            );
+        } catch (\Exception $e) {
+            throw new ApiRequestException(
+                logMessage: 'Unexpected error during invoice expiration: '.$e->getMessage(),
+                userMessage: 'Terjadi kesalahan tidak terduga pada sistem pembayaran, silakan coba beberapa saat lagi.',
+                statusCode: $e->getCode()
+            );
+        }
+    }
+
+    public function createRefund(Order $order)
+    {
+        $params = [
+            'invoice_id' => $order->payment->xendit_invoice_id,
+            'reference_id' => $order->order_number,
+            'amount' => (float) $order->total_amount,
+            'currency' => 'IDR',
+            'reason' => 'CANCELLATION',
+        ];
+
+        $apiInstance = new RefundApi;
+        $createRefund = new CreateRefund($params);
+
+        try {
+            return $apiInstance->createRefund(idempotency_key: $order->payment->refund->id, create_refund: $createRefund);
+        } catch (\Xendit\XenditSdkException $e) {
+            $userMessage = null;
+
+            if ($e->getErrorCode() === 400) {
+                $userMessage = 'Gagal memproses permintaan refund karena total belanja melebihi batas maksimal yang diizinkan untuk pengembalian dana.';
+            } elseif ($e->getErrorCode() === 403) {
+                $userMessage = 'Gagal memproses permintaan refund karena tidak ada API key yang valid.';
+            } elseif ($e->getErrorCode() === 404) {
+                $userMessage = 'Gagal memproses permintaan refund karena data yang diminta tidak ditemukan.';
+            } elseif ($e->getErrorCode() === 409) {
+                $userMessage = 'Gagal memproses permintaan refund karena permintaan refund ini telah diproses.';
+            } elseif ($e->getErrorCode() === 504) {
+                $userMessage = 'Gagal memproses permintaan refund karena saluran pembayaran yang diminta saat ini mengalami masalah, silakan coba beberapa saat lagi.';
+            }
+
+            throw new ApiRequestException(
+                logMessage: 'Unexpected error during xendit refund creation: '.$e->getErrorMessage(),
+                userMessage: $userMessage ?? 'Terjadi kesalahan pada sistem refund, silakan coba beberapa saat lagi.',
+                statusCode: $e->getErrorCode()
+            );
+        } catch (\Exception $e) {
+            throw new ApiRequestException(
+                logMessage: 'Unexpected error during refund creation: '.$e->getMessage(),
+                userMessage: 'Terjadi kesalahan tidak terduga pada sistem refund, silakan coba beberapa saat lagi.',
                 statusCode: $e->getCode()
             );
         }
