eb121b3a4157b61a1fe24461c4242a7fcc06479a
caesariomj <caesariomj7@gmail.com>
2025-01-09 09:19:21 +0700

    feat: add user order link to user navigation component

--- resources/views/livewire/layout/user/navigation.blade.php
+++ resources/views/livewire/layout/user/navigation.blade.php
@@ -219,6 +219,30 @@ new class extends Component {
                                 </svg>
                                 Profil Saya
                             </x-common.dropdown-link>
+
+                            @can('view own orders')
+                                <x-common.dropdown-link :href="route('orders.index')" wire:navigate>
+                                    <svg
+                                        class="size-4"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        stroke-width="2"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                    >
+                                        <path
+                                            d="M11 21.73a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73z"
+                                        />
+                                        <path d="M12 22V12" />
+                                        <path d="m3.3 7 7.703 4.734a2 2 0 0 0 1.994 0L20.7 7" />
+                                        <path d="m7.5 4.27 9 5.15" />
+                                    </svg>
+                                    Pesanan Saya
+                                </x-common.dropdown-link>
+                            @endcan
+
                             <x-common.dropdown-link wire:click="logout" class="text-red-500 hover:bg-red-50">
                                 <svg
                                     class="size-4"
@@ -347,6 +371,30 @@ new class extends Component {
                             </svg>
                             Profil Saya
                         </x-user.responsive-nav-link>
+
+                        @can('view own orders')
+                            <x-user.responsive-nav-link :href="route('orders.index')" wire:navigate>
+                                <svg
+                                    class="size-4"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                >
+                                    <path
+                                        d="M11 21.73a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73z"
+                                    />
+                                    <path d="M12 22V12" />
+                                    <path d="m3.3 7 7.703 4.734a2 2 0 0 0 1.994 0L20.7 7" />
+                                    <path d="m7.5 4.27 9 5.15" />
+                                </svg>
+                                Pesanan Saya
+                            </x-user.responsive-nav-link>
+                        @endcan
+
                         <button wire:click="logout" class="w-full text-start">
                             <x-user.responsive-nav-link class="!text-red-500 hover:border-red-300 hover:bg-red-50">
                                 <svg
