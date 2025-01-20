ee40f9192729288267b9b756e7cd46d8ca4f84aa
caesariomj <caesariomj7@gmail.com>
2025-01-20 16:13:35 +0700

    style: update user order page layout styling

--- resources/views/pages/user/orders/index.blade.php
+++ resources/views/pages/user/orders/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <section class="flex flex-row gap-6 p-4 md:p-6">
         <x-user.sidebar />
-        <section class="w-full md:w-5/6">
+        <section class="w-full shrink">
             <h1 class="mb-4 text-black">Pesanan Saya</h1>
             <livewire:user.order-item-list />
         </section>
