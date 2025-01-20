96bc3fc6bf64cd9d71533c60b4fba47730ace3b8
caesariomj <caesariomj7@gmail.com>
2025-01-20 16:12:26 +0700

    feat: create user account setting side-link, update user sidebar styling, and add permission

--- resources/views/components/user/sidebar.blade.php
+++ resources/views/components/user/sidebar.blade.php
@@ -6,28 +6,30 @@
 @endphp
 
 <aside
-    class="hidden md:sticky md:top-20 md:block md:h-full md:w-1/6 md:rounded md:border md:border-neutral-300 md:p-4 md:shadow-md"
+    class="hidden md:sticky md:top-20 md:block md:h-full md:w-64 md:shrink-0 md:rounded md:border md:border-neutral-300 md:p-4 md:shadow-md"
 >
     <ul class="space-y-1.5">
-        <li>
-            <x-user.side-link :href="route('profile')" :active="request()->routeIs('profile')" wire:navigate>
-                <svg
-                    class="size-5 shrink-0"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    aria-hidden="true"
-                >
-                    <circle cx="12" cy="8" r="5" />
-                    <path d="M20 21a8 8 0 0 0-16 0" />
-                </svg>
-                Profil Saya
-            </x-user.side-link>
-        </li>
+        @can('view own account')
+            <li>
+                <x-user.side-link :href="route('profile')" :active="request()->routeIs('profile')" wire:navigate>
+                    <svg
+                        class="size-5 shrink-0"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        aria-hidden="true"
+                    >
+                        <circle cx="12" cy="8" r="5" />
+                        <path d="M20 21a8 8 0 0 0-16 0" />
+                    </svg>
+                    Profil Saya
+                </x-user.side-link>
+            </li>
+        @endcan
 
         @can('view own orders')
             <li>
@@ -66,6 +68,39 @@
                             {{ $ordersCount }}
                         </span>
                     @endif
+                </x-user.side-link>
+            </li>
+        @endcan
+
+        @can('view own account')
+            <li>
+                <x-user.side-link :href="route('setting')" :active="request()->routeIs('setting')" wire:navigate>
+                    <svg
+                        class="size-5 shrink-0"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        aria-hidden="true"
+                    >
+                        <path d="M2 21a8 8 0 0 1 10.434-7.62" />
+                        <circle cx="10" cy="8" r="5" />
+                        <circle cx="18" cy="18" r="3" />
+                        <path d="m19.5 14.3-.4.9" />
+                        <path d="m16.9 20.8-.4.9" />
+                        <path d="m21.7 19.5-.9-.4" />
+                        <path d="m15.2 16.9-.9-.4" />
+                        <path d="m21.7 16.5-.9.4" />
+                        <path d="m15.2 19.1-.9.4" />
+                        <path d="m19.5 21.7-.4-.9" />
+                        <path d="m16.9 15.2-.4-.9" />
+                    </svg>
+                    Pengaturan Akun
                 </x-user.side-link>
             </li>
         @endcan
