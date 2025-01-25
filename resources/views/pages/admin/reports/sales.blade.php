6c9df9964f8d05f533040053ab489d97681f983d
caesariomj <caesariomj7@gmail.com>
2025-01-25 15:04:14 +0700

    feat: create sales report page and Livewire component

---
+++ resources/views/pages/admin/reports/sales.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+
+@section('title', 'Laporan Penjualan')
+
+@section('content')
+    <section>
+        <div class="mb-4 flex flex-col items-start justify-between gap-4 md:flex-row md:items-center">
+            <h1 class="leading-none text-black">Laporan Penjualan</h1>
+        </div>
+        <livewire:admin.reports.sales-table />
+    </section>
+@endsection
