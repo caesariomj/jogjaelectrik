6594acbebcdb5e962dc9026c5964c2a97d2642fa
caesariomj <caesariomj7@gmail.com>
2025-01-31 20:11:36 +0700

    refactor: enhance category and subcategory detail views

--- resources/views/pages/admin/categories/show.blade.php
+++ resources/views/pages/admin/categories/show.blade.php
@@ -60,13 +60,13 @@
             <div class="flex flex-col items-center gap-1 border-b border-neutral-300 py-2 md:flex-row">
                 <dt class="w-full tracking-tight text-black/70 md:w-1/3">Total Subkategori Terkait</dt>
                 <dd class="w-full font-medium tracking-tight text-black md:w-2/3">
-                    {{ $category->total_subcategories }}
+                    {{ formatPrice($category->total_subcategories) }}
                 </dd>
             </div>
             <div class="flex flex-col items-center gap-1 border-b border-neutral-300 py-2 md:flex-row">
                 <dt class="w-full tracking-tight text-black/70 md:w-1/3">Total Produk Terkait</dt>
                 <dd class="w-full font-medium tracking-tight text-black md:w-2/3">
-                    {{ $category->total_products }}
+                    {{ formatPrice($category->total_products) }}
                 </dd>
             </div>
             <div class="flex flex-col items-center gap-1 border-b border-neutral-300 py-2 md:flex-row">
--- resources/views/pages/admin/subcategories/show.blade.php
+++ resources/views/pages/admin/subcategories/show.blade.php
@@ -25,7 +25,7 @@
             </x-common.button>
             <h1 class="leading-none text-black">Detail Subkategori &mdash; {{ ucwords($subcategory->name) }}</h1>
         </header>
-        <dl class="grid grid-cols-1">
+        <dl class="mb-8 grid grid-cols-1">
             <div class="flex flex-col items-center gap-1 border-b border-neutral-300 py-2 md:flex-row">
                 <dt class="w-full tracking-tight text-black/70 md:w-1/3">Nama Subkategori</dt>
                 <dd class="w-full font-medium tracking-tight text-black md:w-2/3">
@@ -35,13 +35,13 @@
             <div class="flex flex-col items-center gap-1 border-b border-neutral-300 py-2 md:flex-row">
                 <dt class="w-full tracking-tight text-black/70 md:w-1/3">Kategori Terkait</dt>
                 <dd class="w-full font-medium tracking-tight text-black md:w-2/3">
-                    {{ ucwords($subcategory->category->name) }}
+                    {{ ucwords($subcategory->category_name) }}
                 </dd>
             </div>
             <div class="flex flex-col items-center gap-1 border-b border-neutral-300 py-2 md:flex-row">
                 <dt class="w-full tracking-tight text-black/70 md:w-1/3">Total Produk Terkait</dt>
                 <dd class="w-full font-medium tracking-tight text-black md:w-2/3">
-                    {{ formatPrice($subcategory->products_count) }}
+                    {{ formatPrice($subcategory->total_products) }}
                 </dd>
             </div>
             <div class="flex flex-col items-center gap-1 border-b border-neutral-300 py-2 md:flex-row">
@@ -57,5 +57,15 @@
                 </dd>
             </div>
         </dl>
+        <div class="flex flex-col items-center gap-4 md:flex-row md:justify-end">
+            <x-common.button
+                :href="route('admin.subcategories.index')"
+                variant="secondary"
+                class="w-full md:w-fit"
+                wire:navigate
+            >
+                Kembali
+            </x-common.button>
+        </div>
     </section>
 @endsection
