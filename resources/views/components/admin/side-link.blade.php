b6720c2261a02039f126f5a4dd782a201b0ef9c3
caesariomj <caesariomj7@gmail.com>
2025-02-02 22:57:06 +0700

    style: update admin side-link active state styling

--- resources/views/components/admin/side-link.blade.php
+++ resources/views/components/admin/side-link.blade.php
@@ -5,7 +5,7 @@
 @php
     $classes =
         $active ?? false
-            ? 'flex items-center gap-x-3 rounded-lg bg-primary px-4 py-2 text-sm font-medium tracking-tight text-white'
+            ? 'relative flex items-center gap-x-3 rounded-lg bg-primary-50 px-4 py-2 text-sm font-medium tracking-tight text-primary before:absolute before:bottom-0 before:left-0 before:top-1/2 before:h-4 before:w-1 before:-translate-y-1/2 before:rounded-r-md before:bg-primary'
             : 'flex items-center gap-x-3 rounded-lg px-4 py-2 text-sm font-medium tracking-tight text-black transition-all hover:bg-primary-50 hover:text-primary active:ring-2 active:ring-primary-200';
 @endphp
 
