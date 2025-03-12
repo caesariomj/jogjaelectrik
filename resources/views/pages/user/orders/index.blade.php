b5106c9c06e6e3ec10c4be68bf5a134ef1ff6b29
caesariomj <caesariomj7@gmail.com>
2025-03-13 04:28:25 +0700

    feat: add lazy loading to user orders, profile, setting features, update styling

--- resources/views/pages/user/orders/index.blade.php
+++ resources/views/pages/user/orders/index.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'Pesanan Saya')
 
 @section('content')
-    <section class="flex flex-row gap-6 p-4 md:p-6">
+    <section class="container mx-auto flex max-w-md flex-row gap-6 p-6 md:max-w-[96rem] md:p-12">
         <x-user.sidebar />
         <section class="w-full shrink">
-            <h1 class="mb-4 text-black">Pesanan Saya</h1>
-            <livewire:user.order-item-list />
+            <h1 class="mb-6 text-black">Pesanan Saya</h1>
+            <livewire:user.order-item-list lazy />
         </section>
     </section>
 @endsection
--- resources/views/pages/user/setting.blade.php
+++ resources/views/pages/user/setting.blade.php
@@ -3,13 +3,13 @@
 @section('title', 'Pengaturan Akun')
 
 @section('content')
-    <section class="flex flex-row gap-6 p-4 md:p-6">
+    <section class="container mx-auto flex max-w-md flex-row gap-6 p-6 md:max-w-[96rem] md:p-12">
         <x-user.sidebar />
         <section class="w-full shrink">
-            <h1 class="mb-4 text-black">Pengaturan Akun</h1>
-            <livewire:user.profile.update-password-form />
+            <h1 class="mb-6 text-black">Pengaturan Akun</h1>
+            <livewire:user.profile.update-password-form lazy />
             <hr class="my-8 border-neutral-300" />
-            <livewire:user.profile.delete-user-form />
+            <livewire:user.profile.delete-user-form lazy />
         </section>
     </section>
 @endsection
