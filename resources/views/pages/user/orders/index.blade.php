07f6453194151a4b355bdb4a751c3ad89ba4f93b
caesariomj <caesariomj7@gmail.com>
2025-03-21 00:18:29 +0700

    style: update user order page styling

--- resources/views/pages/user/orders/index.blade.php
+++ resources/views/pages/user/orders/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <section class="container mx-auto flex max-w-md flex-row gap-6 p-6 md:max-w-[96rem] md:p-12">
         <x-user.sidebar />
-        <section class="w-full shrink">
+        <section class="w-full shrink md:w-[calc(100%-18rem)]">
             <h1 class="mb-6 text-black">Pesanan Saya</h1>
             <livewire:user.order-item-list lazy />
         </section>
