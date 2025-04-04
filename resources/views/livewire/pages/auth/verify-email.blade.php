6cdb3c2816d2dab241474534d350989bb77fdaa7
caesariomj <caesariomj7@gmail.com>
2025-04-04 11:11:29 +0700

    refactor: improve verify email page

--- resources/views/livewire/pages/auth/verify-email.blade.php
+++ resources/views/livewire/pages/auth/verify-email.blade.php
@@ -1,12 +1,10 @@
 <?php
 
-use App\Livewire\Actions\Logout;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
-new #[Layout('layouts.app')] class extends Component {
+new #[Layout('layouts.auth')] class extends Component {
     /**
      * Send an email verification notification to the user.
      */
@@ -20,42 +18,77 @@ new #[Layout('layouts.app')] class extends Component {
 
         Auth::user()->sendEmailVerificationNotification();
 
-        Session::flash('status', 'verification-link-sent');
-    }
-
-    /**
-     * Log the current user out of the application.
-     */
-    public function logout(Logout $logout): void
-    {
-        $logout();
-
-        $this->redirect('/', navigate: true);
+        session()->flash('status', 'Link verifikasi baru telah dikirim ke alamat email Anda.');
     }
 }; ?>
 
-<div>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-    </div>
+@section('title', 'Verifikasi Email')
 
-    @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 text-sm font-medium text-green-600">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+<div class="w-full">
+    <div class="mb-3 inline-flex items-center gap-x-2">
+        <x-common.button
+            :href="route('home')"
+            variant="secondary"
+            class="!p-2 md:hidden"
+            aria-label="Kembali ke halaman sebelumnya"
+            wire:navigate
+        >
+            <svg
+                class="size-4 shrink-0"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.8"
+                stroke="currentColor"
+            >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+            </svg>
+        </x-common.button>
+        <h1 class="text-4xl font-bold text-black">Verifikasi Email</h1>
+    </div>
+    <p class="mb-6 text-base tracking-tight text-black/70">
+        Terima kasih telah mendaftar! Sebelum mulai berbelanja, Anda perlu memverifikasi alamat email Anda dengan
+        mengklik tautan yang baru saja kami kirimkan ke email Anda. Jika Anda tidak menerima email tersebut, Anda dapat
+        mengklik tombol dibawah ini untuk mengirimkan ulang link verifikasi ke email Anda.
+    </p>
+
+    @if (session('status'))
+        <div
+            class="mt-3 flex items-center gap-x-2 rounded-lg border border-teal-200 bg-teal-100 p-4 text-sm font-medium tracking-tight text-teal-800"
+            role="alert"
+            tabindex="-1"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-4 shrink-0"
+                aria-hidden="true"
+            >
+                <path
+                    fill-rule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                    clip-rule="evenodd"
+                />
+            </svg>
+            {{ session('status') }}
         </div>
     @endif
 
-    <div class="mt-4 flex items-center justify-between">
-        <x-common.button variant="primary" wire:click="sendVerification">
-            {{ __('Resend Verification Email') }}
+    <div class="mt-8 flex flex-col items-center gap-y-4">
+        <x-common.button variant="primary" class="w-full" wire:click="sendVerification">
+            <span wire:loading.remove wire:target="sendVerification">Kirim ulang email verifikasi</span>
+            <div
+                class="ms-1 inline-block size-4 animate-spin rounded-full border-[3px] border-current border-t-transparent"
+                role="status"
+                aria-label="loading"
+                wire:loading
+                wire:target="sendVerification"
+            ></div>
+            <span wire:loading wire:target="sendVerification">Sedang Diproses...</span>
         </x-common.button>
-
-        <button
-            wire:click="logout"
-            type="submit"
-            class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        >
-            {{ __('Log Out') }}
-        </button>
+        <x-common.button :href="route('home')" variant="secondary" class="w-full" wire:navigate>
+            Kembali
+        </x-common.button>
     </div>
 </div>
