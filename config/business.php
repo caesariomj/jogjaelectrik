74dcf651bcb7b8f4b2a72932da1b068ff75da272
caesariomj <caesariomj7@gmail.com>
2025-04-09 13:07:24 +0700

    feat: create business config to take static business data from .env

---
+++ config/business.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'address' => env('APP_BUSINESS_ADDRESS', '-'),
+    'map_link' => env('APP_BUSINESS_MAP_LINK', 'https://www.google.com/maps'),
+    'phone' => env('APP_BUSINESS_PHONE', '-'),
+    'whatsapp' => env('APP_BUSINESS_WHATSAPP', 'https://wa.me/'),
+    'email' => env('APP_BUSINESS_EMAIL', '-'),
+    'facebook' => env('APP_BUSINESS_FACEBOOK', 'https://www.facebook.com/'),
+    'instagram' => env('APP_BUSINESS_INSTAGRAM', 'https://www.instagram.com/'),
+];
