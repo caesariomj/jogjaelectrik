4149432c06032ffd915dbbb74c83a4bde0865d69
caesariomj <caesariomj7@gmail.com>
2025-04-09 13:09:00 +0700

    feat: add fragment identifier inside faqAccordion component

--- resources/js/components/faqAccordion.js
+++ resources/js/components/faqAccordion.js
@@ -2,6 +2,14 @@ export default (items) => ({
     search: '',
     items: items,
 
+    init() {
+        let hash = window.location.hash.substring(1);
+
+        if (hash) {
+            this.search = decodeURIComponent(hash);
+        }
+    },
+
     get filteredItems() {
         return this.items.filter(
             (item) =>
