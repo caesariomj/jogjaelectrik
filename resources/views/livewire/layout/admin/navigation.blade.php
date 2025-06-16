e146ab768ccf3d8ec05ed5311779b00570c38adb
caesariomj <caesariomj7@gmail.com>
2025-06-16 17:34:00 +0700

    style: remove notification button from admin navigation component

--- resources/views/livewire/layout/admin/navigation.blade.php
+++ resources/views/livewire/layout/admin/navigation.blade.php
@@ -34,27 +34,6 @@ new class extends Component {
             </svg>
         </button>
         <div class="flex items-center space-x-2">
-            <button type="button" class="relative rounded-full p-2 text-black hover:bg-neutral-100">
-                <svg
-                    class="size-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                >
-                    <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
-                    <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
-                </svg>
-                <span class="sr-only">notifikasi masuk</span>
-                <div
-                    class="absolute -end-2 -top-2 inline-flex size-6 items-center justify-center rounded-full border border-white bg-red-500 text-xs font-semibold text-white"
-                >
-                    9+
-                </div>
-            </button>
             <div class="flex items-center ps-4">
                 <x-common.dropdown align="right" width="48">
                     <x-slot name="trigger">
