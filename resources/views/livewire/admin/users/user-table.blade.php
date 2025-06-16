4ce9fa682fb279c9651768377d3ca0c90e210acb
caesariomj <caesariomj7@gmail.com>
2025-06-16 10:41:16 +0700

    feat: add edit user dropdown button

--- resources/views/livewire/admin/users/user-table.blade.php
+++ resources/views/livewire/admin/users/user-table.blade.php
@@ -282,6 +282,31 @@ new class extends Component {
                                     </x-common.dropdown-link>
                                 @endcan
 
+                                @can('edit all accounts')
+                                    <x-common.dropdown-link
+                                        :href="route('admin.users.edit', ['id' => $user->id])"
+                                        wire:navigate
+                                    >
+                                        <svg
+                                            class="size-4"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            stroke="currentColor"
+                                            stroke-width="2"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            aria-hidden="true"
+                                        >
+                                            <path
+                                                d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"
+                                            />
+                                            <path d="m15 5 4 4" />
+                                        </svg>
+                                        Ubah
+                                    </x-common.dropdown-link>
+                                @endcan
+
                                 @can('delete all accounts')
                                     <x-common.dropdown-link
                                         x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion-{{ $user->id }}')"
