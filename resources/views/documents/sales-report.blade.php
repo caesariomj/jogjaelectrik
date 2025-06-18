44a4ec706fcb151dccfd8b8a9704efdefe7cbff3
caesariomj <caesariomj7@gmail.com>
2025-06-18 14:28:51 +0700

    style: update sales report styling

--- resources/views/documents/sales-report.blade.php
+++ resources/views/documents/sales-report.blade.php
@@ -26,60 +26,162 @@
                 letter-spacing: -0.025em;
             }
 
+            h1 {
+                font-size: 20px;
+                font-weight: bolder;
+                margin-top: 10px;
+                margin-bottom: 16px;
+            }
+
+            h2 {
+                font-size: 20px;
+                font-weight: bolder;
+                margin-bottom: 8px;
+            }
+
+            .container {
+                width: 100%;
+                margin-bottom: 16px;
+            }
+
+            .left {
+                float: left;
+                width: 60%;
+            }
+
+            .right {
+                float: right;
+                width: 35%;
+                text-align: right;
+            }
+
+            .label {
+                font-weight: bold;
+            }
+
+            .value {
+                margin-bottom: 10px;
+            }
+
+            .clear {
+                clear: both;
+            }
+
             table {
                 width: 100%;
                 border-collapse: collapse;
             }
 
-            th,
-            td {
-                padding: 8px;
+            table thead tr th {
+                background-color: #fd5722;
+                border: 1px solid #000000;
+                color: white;
                 text-align: left;
-                border-bottom: 1px solid #ddd;
+                padding: 5px;
+                font-weight: bold;
             }
 
-            th {
-                background-color: #f2f2f2;
+            table tbody tr,
+            table tbody td {
+                border: 1px solid #000000;
+            }
+
+            table tbody tr td {
+                padding: 8px 4px;
             }
         </style>
     </head>
     <body>
-        <h1>LAPORAN PENJUALAN (Waktu)</h1>
+        @php
+            $svg = '
+                <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 316 316" fill="#fd5722">
+                    <polygon
+                        points="215.24 70.31 316.24 105.47 316.24 52.84 165.24 0.3 165.24 18.92 165.24 53.02 165.24 105.37 165.24 158.09 165.24 210.44 165.24 221.13 165.24 263.16 316.24 315.69 316.24 262.86 215.24 227.7 215.24 175.42 316.24 210.4 316.24 157.94 215.24 122.78 215.24 70.31"
+                    />
+                    <polygon
+                        points="0 52.64 0 105.27 101.33 70 101.33 70 101.33 227.9 51.02 245.45 51.02 210.73 0 227.9 0 263.18 0 316 151.29 263.36 151.29 221.25 151.29 210.54 151.29 52.82 151.29 18.65 151.29 0 0 52.64"
+                    />
+                </svg>
+            ';
+
+            $image = '<img src="data:image/svg+xml;base64,' . base64_encode($svg) . '"  width="50" height="50" />';
+        @endphp
+
+        {!! $image !!}
+
+        <h1>
+            LAPORAN PENJUALAN
+
+            @if ($month)
+                {{ $month }}
+            @endif
+
+            {{ $year }}
+        </h1>
         <h2>RINGKASAN PENJUALAN</h2>
-        <p>TANGGAL: TANGGAL BERAPA - SAMPAI TANGGAL BERAPA</p>
-        <p>SUBTOTAL PENJUALAN: Subtotalnya Berapa Ya</p>
-        <p>TOTAL DISKON: Total diskon berapa ya</p>
-        <p>TOTAL BIAYA PENGIRIMAN: Total biaya pengiriman berapa ya</p>
-        <p>TOTAL PENJUALAN: Total penjualan berapa ya</p>
-        <h2>TABEL PENJUALAN</h2>
+        <div class="container">
+            <div class="left">
+                <p>
+                    Periode laporan penjualan:
+                    <strong>
+                        @if ($month)
+                            {{ 'Bulan ' . $month . ', ' }}
+                        @endif
+
+                        {{ 'Tahun ' . $year }}
+                    </strong>
+                </p>
+                <p>
+                    Laporan ini dibuat pada:
+                    <strong>{{ formatTimestamp(now()) }}</strong>
+                </p>
+            </div>
+            <div class="right">
+                <div class="value total-line">
+                    <span class="label">GRAND TOTAL PENJUALAN</span>
+                    <br />
+                    <strong>Rp {{ formatPrice($grandTotal) }}</strong>
+                </div>
+            </div>
+
+            <div class="clear"></div>
+        </div>
         <table>
             <thead>
                 <tr>
                     <th>NO.</th>
-                    <th>NOMOR PESANAN</th>
-                    <th>TANGGAL</th>
-                    <th>NAMA PEMBELI</th>
-                    <th>STATUS PESANAN</th>
-                    <th>TOTAL</th>
-                    <th>METODE PEMBAYARAN</th>
+                    <th>NAMA PRODUK</th>
+                    <th>VARIASI</th>
+                    <th>KATEGORI / SUBKATEGORI</th>
+                    <th align="center">TERJUAL</th>
+                    <th>HARGA SATUAN</th>
+                    <th>TOTAL PENJUALAN</th>
                 </tr>
             </thead>
             <tbody>
-                @foreach ($sales as $sale)
+                @foreach ($products as $product)
                     <tr>
-                        <td>{{ $loop->index + 1 . '.' }}</td>
-                        <td>{{ $sale->order_number }}</td>
-                        <td>{{ $sale->created_at->format('d-m-Y') }}</td>
-                        <td>{{ $sale->user->name }}</td>
-                        <td>Sukses</td>
-                        <td>{{ formatPrice($sale->total_amount) }}</td>
+                        <td>{{ $loop->iteration }}.</td>
                         <td>
-                            @if (str_contains($sale->payment->method, 'bank_transfer'))
-                                {{ strtoupper(str_replace('bank_transfer_', '', $sale->payment->method)) }}
+                            {{ $product['name'] }}
+                        </td>
+                        <td>
+                            @if ($product['variation_name'] && $product['variant_name'])
+                                {{ ucwords($product['variation_name']) . ' : ' . ucwords($product['variant_name']) }}
                             @else
-                                {{ strtoupper(str_replace('ewallet_', '', $sale->payment->method)) }}
+                                -
                             @endif
                         </td>
+                        @if ($product['subcategory_name'] && $product['category_name'])
+                            <td>
+                                {{ ucwords($product['subcategory_name']) . ' / ' . ucwords($product['category_name']) }}
+                            </td>
+                        @else
+                            <td>-</td>
+                        @endif
+                        <td align="center">{{ formatPrice($product['total_sold']) }}</td>
+                        <td>Rp {{ formatPrice($product['price']) }}</td>
+                        <td>Rp {{ formatPrice($product['total_sales']) }}</td>
                     </tr>
                 @endforeach
             </tbody>
