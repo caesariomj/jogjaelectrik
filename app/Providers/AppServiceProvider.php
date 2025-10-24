cbda548ebf3ce758d9a62fd5f60a7691c157ce26
caesariomj <caesariomj7@gmail.com>
2025-10-25 01:04:34 +0700

    fix: Uncomment AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,40 +37,40 @@ class AppServiceProvider extends ServiceProvider
 
         $currentRoute = $this->app->request->getRequestUri();
 
-        // if (! str_contains($currentRoute, 'admin')) {
-        //     $primaryCategories = Category::queryPrimaryWithSubcategories(
-        //         columns: [
-        //             'categories.id',
-        //             'categories.name',
-        //             'categories.slug',
-        //         ])
-        //         ->orderBy('categories.name')
-        //         ->get()
-        //         ->groupBy(function ($category) {
-        //             return $category->id;
-        //         })
-        //         ->take(2)
-        //         ->map(function ($grouppedCategories) {
-        //             $category = $grouppedCategories->first();
+        if (! str_contains($currentRoute, 'admin')) {
+            $primaryCategories = Category::queryPrimaryWithSubcategories(
+                columns: [
+                    'categories.id',
+                    'categories.name',
+                    'categories.slug',
+                ])
+                ->orderBy('categories.name')
+                ->get()
+                ->groupBy(function ($category) {
+                    return $category->id;
+                })
+                ->take(2)
+                ->map(function ($grouppedCategories) {
+                    $category = $grouppedCategories->first();
 
-        //             $subcategories = $grouppedCategories->map(function ($subcategory) {
-        //                 return (object) [
-        //                     'id' => $subcategory->subcategory_id,
-        //                     'name' => $subcategory->subcategory_name,
-        //                     'slug' => $subcategory->subcategory_slug,
-        //                 ];
-        //             });
+                    $subcategories = $grouppedCategories->map(function ($subcategory) {
+                        return (object) [
+                            'id' => $subcategory->subcategory_id,
+                            'name' => $subcategory->subcategory_name,
+                            'slug' => $subcategory->subcategory_slug,
+                        ];
+                    });
 
-        //             return (object) [
-        //                 'id' => $category->id,
-        //                 'name' => $category->name,
-        //                 'slug' => $category->slug,
-        //                 'subcategories' => $subcategories->values(),
-        //             ];
-        //         })
-        //         ->values();
+                    return (object) [
+                        'id' => $category->id,
+                        'name' => $category->name,
+                        'slug' => $category->slug,
+                        'subcategories' => $subcategories->values(),
+                    ];
+                })
+                ->values();
 
-        //     View::share('primaryCategories', $primaryCategories);
-        // }
+            View::share('primaryCategories', $primaryCategories);
+        }
     }
 }
