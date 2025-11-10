ca3531771294f3806b2dafedd1010db850121501
caesariomj <caesariomj7@gmail.com>
2025-11-11 06:09:32 +0700

    fix: Prevent DB query execution when running in console in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use App\Models\Category;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
@@ -29,48 +28,44 @@ class AppServiceProvider extends ServiceProvider
                 ->numbers();
         });
 
-        // Gate::before(function ($user, $ability) {
-        //     if ($user->hasRole('super_admin')) {
-        //         return true;
-        //     }
-        // });
-
         $currentRoute = $this->app->request->getRequestUri();
 
-        if (! str_contains($currentRoute, 'admin')) {
-            $primaryCategories = Category::queryPrimaryWithSubcategories(
-                columns: [
-                    'categories.id',
-                    'categories.name',
-                    'categories.slug',
-                ])
-                ->orderBy('categories.name')
-                ->get()
-                ->groupBy(function ($category) {
-                    return $category->id;
-                })
-                ->take(2)
-                ->map(function ($grouppedCategories) {
-                    $category = $grouppedCategories->first();
+        if (! $this->app->runningInConsole()) {
+            if (! str_contains($currentRoute, 'admin')) {
+                $primaryCategories = Category::queryPrimaryWithSubcategories(
+                    columns: [
+                        'categories.id',
+                        'categories.name',
+                        'categories.slug',
+                    ])
+                    ->orderBy('categories.name')
+                    ->get()
+                    ->groupBy(function ($category) {
+                        return $category->id;
+                    })
+                    ->take(2)
+                    ->map(function ($grouppedCategories) {
+                        $category = $grouppedCategories->first();
 
-                    $subcategories = $grouppedCategories->map(function ($subcategory) {
+                        $subcategories = $grouppedCategories->map(function ($subcategory) {
+                            return (object) [
+                                'id' => $subcategory->subcategory_id,
+                                'name' => $subcategory->subcategory_name,
+                                'slug' => $subcategory->subcategory_slug,
+                            ];
+                        });
+
                         return (object) [
-                            'id' => $subcategory->subcategory_id,
-                            'name' => $subcategory->subcategory_name,
-                            'slug' => $subcategory->subcategory_slug,
+                            'id' => $category->id,
+                            'name' => $category->name,
+                            'slug' => $category->slug,
+                            'subcategories' => $subcategories->values(),
                         ];
-                    });
+                    })
+                    ->values();
 
-                    return (object) [
-                        'id' => $category->id,
-                        'name' => $category->name,
-                        'slug' => $category->slug,
-                        'subcategories' => $subcategories->values(),
-                    ];
-                })
-                ->values();
-
-            View::share('primaryCategories', $primaryCategories);
+                View::share('primaryCategories', $primaryCategories);
+            }
         }
     }
 }
